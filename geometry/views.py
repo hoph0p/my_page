2e049abf8fac58e02735738804340dfff4111f41
Hop_Hop <novikov.hop.work@yandex.ru>
2022-06-27 17:33:25 +0300

    some edit in geometry app

--- geometry/views.py
+++ geometry/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 
 # Create your views here.
 
 def get_rectangle_area(request, width: int, height: int):
-    return HttpResponse(f'Площадь прямоугольника размером {width}x{height} равна {width * height}')
+    result_area = width * height
+    return HttpResponse(f'Площадь прямоугольника размером {width}x{height} равна {result_area}')
 
 
 def get_square_area(request, width: int):
-    return HttpResponse(f'Площадь квадрата размером {width}x{width} равна {width ** 2}')
+    result_area = width ** 2
+    return HttpResponse(f'Площадь квадрата размером {width}x{width} равна {result_area}')
 
 
 def get_circle_area(request, radius: int):
-    return HttpResponse(f'Площадь круга радиусом {radius} равна {3.14 * (radius ** 2)}')
+    result_area = 3.14 * (radius ** 2)
+    return HttpResponse(f'Площадь круга радиусом {radius} равна {result_area}')
 
 
 def rectangle_redirect(request, width: int, height: int):
@@ -26,3 +30,5 @@ def square_redirect(request, width: int):
 
 def circle_redirect(request, radius: int):
     return HttpResponseRedirect(f'/calculate_geometry/circle/{radius}')
+
+
