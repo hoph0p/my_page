0bdf9110073ec4bceeb81927709d55e6be7be7e2
Hop_Hop <novikov.hop.work@yandex.ru>
2022-06-30 13:33:45 +0300

    another commit

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='horoscope-index'),
     path('type/', views.type_index),
     path('type/<str:element>', views.get_element, name='horoscope-type'),
     path('<int:sign_zodiac>/', views.get_info_about_sign_zodiac_by_number),
